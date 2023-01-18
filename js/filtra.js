7a81ce357f87b9af49c53c3023d37b86c89f26fc
Wendel Paes <107985828+WendelPaesx@users.noreply.github.com>
2023-01-18 04:36:37 -0300

    .

---
+++ js/filtra.js
@@ -0,0 +1,30 @@
+var campoDeFiltro = document.querySelector("#filtrar-tabela")
+
+campoDeFiltro.addEventListener("input", function () {
+    console.log(this.value);
+    
+    var pacientes = document.querySelectorAll(".paciente")
+
+    if(this.value.length > 0){
+
+        for (var i = 0; i < pacientes.length; i++) {
+        var paciente = pacientes[i]
+        var tdNome = paciente.querySelector(".info-nome")
+        var nome = tdNome.textContent
+        if(nome != this.value){
+            paciente.classList.add("invisivel")
+        }else{
+            paciente.classList.remove("invisivel")
+        }
+    }
+    }else{ for (let i = 0; i < pacientes.length; i++) {
+        const paciente = pacientes[i];
+        paciente.classList.remove("invisivel")
+        
+    }
+
+    }
+
+    
+    
+})
