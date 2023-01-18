ac85cae34256efd2be641863e92985038ebae89e
Wendel Paes <107985828+WendelPaesx@users.noreply.github.com>
2023-01-18 04:00:59 -0300

    .

--- js/remover.js
+++ js/remover.js
@@ -1,11 +1,12 @@
-var pacientes = document.querySelectorAll(".paciente")
-
-
 var tabela = document.querySelector("table")
 
-tabela.addEventListener("dblclick", function(event){
-    var alvoEvento = event.target
-    var paiDoAlvo = alvoEvento.parentNode
-paiDoAlvo.remove()
+tabela.addEventListener("dblclick", function (event) {
+    
+    event.target.parentNode.classList.add("fadeout")
+    setTimeout(function(){
+        event.target.parentNode.remove()
+    },500)
+
+    
 })
 
